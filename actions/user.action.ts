397e89cd8a4749d75d360e9be6a304ffae0beb3e
Gourav Manjhi <gouravmanjhi9313@gmail.com>
2026-04-07 21:12:38 +0530

    post card added.

--- actions/user.action.ts
+++ actions/user.action.ts
@@ -33,3 +33,29 @@ export async function syncUser() {
     console.error("Error syncing user:", error);
   }
 }
+
+export async function getUserByClerkId(clerkId: string) {
+  return prisma.user.findUnique({
+    where: {
+      clerkId,
+    },
+    include: {
+      _count: {
+        select: {
+          followers: true,
+          following: true,
+          posts: true,
+        },
+      },
+    },
+  });
+}
+
+export async function getDbUserId(){
+  const { userId: clerkId } = await auth();
+  if(!clerkId) throw new Error("Unauthorized");
+
+  const user = await getUserByClerkId(clerkId);
+  if(!user) throw new Error("User not found");
+  return user.id;
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/components/theme-provider";
 import Navbar from "@/components/Navbar";
+import Sidebar from "@/components/sidebar";
+import { Toaster } from "react-hot-toast";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -39,16 +41,19 @@ export default function RootLayout({
           >
             <div className="min-h-screen">
               <Navbar />
-              <main className="py-8">
+                 <main className="py-8">
                 {/* container to center the content */}
                 <div className="max-w-7xl mx-auto px-4">
-                  <div className="grid grid-cols-1 lg-grid-cols-12 gap-6">
-                    <div className="hidden lg-block lg-col-span-3">sidebar</div>
-                    <div className="lg-col-span-9">{children}</div>
+                  <div className="grid grid-cols-1 lg:grid-cols-12 gap-6">
+                    <div className="hidden lg:block lg:col-span-3">
+                      <Sidebar />
+                    </div>
+                    <div className="lg:col-span-9">{children}</div>
                   </div>
                 </div>
               </main>
             </div>
+            <Toaster/>
           </ThemeProvider>
         </ClerkProvider>
       </body>
